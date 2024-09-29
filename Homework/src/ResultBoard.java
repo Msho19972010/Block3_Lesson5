540f16c67ba099509a2e9485af20821f155e8bbc
Misha <98160546+Msho19972010@users.noreply.github.com>
2024-09-29 23:48:40 +0200

    Update ResultBoard.java
    Changed the data structure of the studentsDatabase and updated the function "top3".

--- Homework/src/ResultBoard.java
+++ Homework/src/ResultBoard.java
@@ -1,26 +1,37 @@
 import java.util.*;
 
 public class ResultBoard {
-    TreeMap<Float, String> studentsDataBase = new TreeMap<>();
+    StudentComparator comparator = new StudentComparator();
+    TreeSet<Student> studentsDataBase = new TreeSet<>(comparator);
     List<String> topThreeStudents = new ArrayList<>(3);
 
-    void addStudent(String name, Float score) {
-        studentsDataBase.put(score, name);
 
-        System.out.println(studentsDataBase);
+    void addStudent(String nameAndSurname, Float score) {
+        Student student = new Student(nameAndSurname, score);
+        
+        studentsDataBase.add(student);
     }
 
     List<String> top3 () {
 
-        Object[] scores = studentsDataBase.keySet().stream().sorted().toArray();
+        Student theBestStudent = studentsDataBase.last();
 
-        for(int i = scores.length - 1; i > scores.length - 4; i--) {
+        if(studentsDataBase.size() == 1) {
+            topThreeStudents.add(studentsDataBase.first().nameAndSurname());
 
-            for(Float student : studentsDataBase.keySet()) {
-                String studentName = studentsDataBase.get(student);
-                if( student.equals(scores[i])) {
-                    topThreeStudents.add(studentName);
-                }
+        } else if(studentsDataBase.size() == 2) {
+            topThreeStudents.add(studentsDataBase.last().nameAndSurname());
+            topThreeStudents.add(studentsDataBase.first().nameAndSurname());
+
+        } else if(studentsDataBase.size() == 3) {
+            topThreeStudents.add(theBestStudent.nameAndSurname());
+            topThreeStudents.add(studentsDataBase.lower(theBestStudent).nameAndSurname());
+            topThreeStudents.add(studentsDataBase.first().nameAndSurname());
+
+        } else {
+            for(int i = 0; i < 3; i++) {
+                topThreeStudents.add(theBestStudent.nameAndSurname());
+                theBestStudent = studentsDataBase.lower(theBestStudent);
             }
         }
 
@@ -30,15 +41,29 @@ public class ResultBoard {
 
     public static void main (String[] args) {
         ResultBoard resultBoard = new ResultBoard();
-        resultBoard.addStudent("Misha", 3.5f);
-        resultBoard.addStudent("Masha", 4.7f);
-        resultBoard.addStudent("Sasha", 4.3f);
-        resultBoard.addStudent("Dasha", 3.7f);
-        resultBoard.addStudent("Natasha", 2.5f);
-        resultBoard.addStudent("Vasia", 4.9f);
-        resultBoard.addStudent("Hass", 3.0f);
+        resultBoard.addStudent("Eva Volt", 3.5f);
+        resultBoard.addStudent("Misha Simonian`", 3.5f);
+        resultBoard.addStudent("Masha Mishiniova", 4.7f);
+        resultBoard.addStudent("Sasha Seraya", 4.3f);
+        resultBoard.addStudent("Dasha Chernaia", 3.7f);
+        resultBoard.addStudent("Natasha Pereviznik", 2.5f);
+        resultBoard.addStudent("Misha Simonian", 4.9f);
+        resultBoard.addStudent("Hass Benz", 3.0f);
 
         System.out.println(resultBoard.top3());
+    }
+
+    public static class StudentComparator implements Comparator<Student> {
+        @Override
+        public int compare(Student student1, Student student2) {
+            if(student1.score() == student2.score()) {
+                return 0;
+            } else if (student1.score() > student2.score()) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
     }
 
 }
